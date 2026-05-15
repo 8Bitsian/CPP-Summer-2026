565a2fccc86fa95beffcc5884575132124f383cb
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-05-15 11:24:54 -0400

    Create FileHead.cpp

---
+++ lesson01/FileHead.cpp
@@ -0,0 +1,20 @@
+/*
+  Imani Hollie 05/15/2026
+  Programming Challenge: File Head Program
+  Write a program that asks the user for the name of a file. The program should display
+  the first ten lines of the file on the screen (the "head" of the file). If the file has
+  fewer than ten lines, the entire file should be displayed, with a message indicating the
+  entire file has been displayed.
+
+  NOTE: Using an editor, you should create a simple text file that can be used to test this program.
+*/
+
+#include <iostream>  // Allows use of input/output objects (cout, cin)
+#include <iomanip>   // Allows use of formatting tools
+#include <string>    // Allows use of the string data type
+using namespace std; // Allows use of the standard library w/out std:: prefix
+
+int main() {
+  // Call functions
+  return 0;  // End program
+}
