962bfbcb25de08a9fd5e555db7a48e394051887b
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-06-15 23:49:03 -0400

    Create C202_2.cpp

---
+++ lesson02/C202_2.cpp
@@ -0,0 +1,79 @@
+/*
+   Imani Hollie 05/30/2026
+   Lesson 2 Lab: Circle Calculator
+   Directions: Use a class named Circle to prompt the user to
+   input the radius of the circle.
+
+   The Table class should include the following:
+      1) Private class member variables for the circles's dimensions
+         - radius: A private member variable to store the circle's radius
+         - PI: A private constant initialized to 3.14159
+      2) Include methods to input the radius and display calculations
+         - Default constructor that initializes the radius to 0.0
+         - Setter function to update the radius value
+         - Getter function to return the circle's radius
+         - Methods to compute and return the following:
+            - Area = PI * radius * radius
+            - Diameter = radius * 2
+            - Circumference = 2 * PI * radius
+      3) Place both the Table class and the main() function in
+         a single file for simplicity
+
+   Sample Output:
+   Radius? 5
+
+   Radius: 5.00
+   Area: 78.54 square units
+   Diameter: 10.00 units
+   Circumference: 31.42 units
+*/
+
+// HEADER
+#include <iostream>     // Provides console input/output
+#include <string>       // Enables use of std::string data type
+#include <regex>        // Enables regular expressions to check text
+#include <iomanip>      // Enables use of formatting tools (not used in this program)
+#include <cstdio>       // Enables C-style printing functions like printf
+using namespace std;    // Enables use of standard names w/out std:: prefix
+
+// CLASSES
+// ClassName{} : Description
+class ClassName {
+public:
+	// methodName() : Description
+	datatype methodName() {
+		// Input/Validate/Return
+   }
+};
+
+// FUNCTION PROTOTYPES
+datatype methodName(dataType parameter, etc.);
+
+// WRAPPER METHODS (PROCESS)
+// wrapMethod() : Description
+datatype wrapMethod() {
+	// Input/Validate/Return
+}
+
+// MAIN METHOD - main() : Program execution starts here
+int main() {
+	// Declare objects
+
+   while (true) {
+      // Input/Process
+      // Read/Write/Output
+   }
+
+   return 0;
+}
+
+// HELPER METHODS (GET & SET)
+// getMethod() : Description
+datatype getMethod() {
+	// Input/Validate/Return
+}
+
+// setMethod() : Description
+datatype setMethod() {
+	// Input/Validate/Return
+}
