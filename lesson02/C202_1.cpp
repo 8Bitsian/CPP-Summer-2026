7e83b45fd5a284e7e33834f94e830a00d19a1ce3
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-06-15 23:48:37 -0400

    Create C202_1.cpp

---
+++ lesson02/C202_1.cpp
@@ -0,0 +1,73 @@
+/*
+   Imani Hollie 05/30/2026
+   Lesson 2 Lab: Table's Surface Area
+   Directions: Use a class named Table to prompt the user to
+   input the length and width of a table (in feet).
+
+   The Table class should include the following:
+      1) Private class member variables for the table's dimensions
+         and methods for calculations
+      2) Include methods to input dimensions and display calculations
+         - Calculate the surface area using: Surface Area = Length * Width
+         - Compute the following:
+            - Cost = Surface Area * 15
+            - Price = Surface Area * 25
+            - Profit = Price - Cost
+      3) Place both the Table class and the main() function in
+         a single file for simplicity
+*/
+
+// HEADER
+#include <iostream>     // Provides console input/output
+#include <string>       // Enables use of std::string data type
+#include <regex>        // Enables regular expressions to check text
+#include <iomanip>      // Enables use of formatting tools (not used in this program)
+#include <cstdio>       // Enables C-style printing functions like printf
+using namespace std;    // Enables use of standard names w/out std:: prefix
+
+// CLASSES
+// ClassName{} : Description
+class Table {
+private:
+	// methodName() : Description
+	datatype methodName() {
+		// Input/Validate/Return
+   }
+public:
+	// methodName() : Description
+	datatype methodName() {
+		// Input/Validate/Return
+   }
+};
+
+// FUNCTION PROTOTYPES
+datatype methodName(dataType parameter, etc.);
+
+// WRAPPER METHODS (PROCESS)
+// wrapMethod() : Description
+datatype wrapMethod() {
+	// Input/Validate/Return
+}
+
+// MAIN METHOD - main() : Program execution starts here
+int main() {
+	// Declare objects
+
+   while (true) {
+      // Input/Process
+      // Read/Write/Output
+   }
+
+   return 0;
+}
+
+// HELPER METHODS (GET & SET)
+// getMethod() : Description
+datatype getMethod() {
+	// Input/Validate/Return
+}
+
+// setMethod() : Description
+datatype setMethod() {
+	// Input/Validate/Return
+}
