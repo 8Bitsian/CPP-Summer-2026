def8a5db7c76f047b6fb8b7f5df7306d0420cd61
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-06-15 20:40:04 -0400

    Update C201_1.cpp

--- lesson01/C201_1.cpp
+++ lesson01/C201_1.cpp
@@ -1,79 +1,92 @@
 /*
     Imani Hollie 05/28/2026
     5-Minute Interview: My Information
-    Directions: Write your full name followed by the last
-    four digits of your cell phone number.
+    Directions: Create a file named myInfo.txt and write
+    your full name and the last four digits of your phone
+    number, then close the file after writing the data.
 */
 
-// Header
-#include <iostream>   // Provides console input/output
-#include <string>     // Enables use of std::string
-#include <regex>      // Enables regualr expressions to check text
-using namespace std;  // Allows use of standard names without std:: prefix
+// HEADER
+#include <iostream>     // Provides console input/output
+#include <fstream>      // Provides file input/output
+#include <string>       // Enables use of std::string
+#include <regex>        // Enables regular expressions to check text
+using namespace std;    // Enables use of standard names w/out std:: prefix
 
+// CLASSES
 // Person{} : Person class collects object name and phone
 class Person {
-  // getName() : Get user input for name
-  public string getName() {
-    // Declare variable
-    string name;
-    
-    while (true) {
-      // Get user input
-      cout << "\nEnter Name (-1 to Exit): ";
-      getline(cin, name);
+public:
+    // getName() : Get user input for full name
+    string getName() {
+        // Declare variables
+        string name;
 
-      // Check for sentinel
-      if (name == "-1") return "-1";
+        while (true) {
+            // Get user input
+            cout << "Enter Name (-1 to Exit): ";
+            getline(cin, name);
 
-      // Validate input
-      if (regex_match(name, regex("[a-zA-Z\\- ]+"))) {
-        return name;
-      } else {
-        cout << "ERROR: Invalid Input - Name must be characters only (a-z or A-Z)\n\n";
-      }
+            // Check for sentinel (-1)
+            if (name == "-1") return "-1";
+
+            // Validate input
+            if (regex_match(name, regex("[a-zA-Z\\- ]+"))) {
+                return name;
+            } else {
+                cout << "ERROR: Invalid Input - Name must be characters only (a-z or A-Z)\n\n";
+            }
+        }
     }
-  }
 
-  // getPhone() : Get user input for last 4-digit phone number
-  public string getPhone() {
-    // Declare variable
-    string phone;
-    
-    while (true) {
-      // Get user input
-      cout << "\nEnter Phone (-1 to Exit): ";
-      getline(cin, phone);
+    // getPhone() : Get user input for last four digits of phone number
+    string getPhone() {
+        // Declare variables
+        string phone;
 
-      // Check for sentinel
-      if (phone == "-1") return "-1";
+        while (true) {
+            // Get user input
+            cout << "Enter Phone (-1 to Exit):";
+            getline(cin, phone);
 
-      // Validate input
-      if (regex_match(phone, regex("[0-9]{4}"))) {
-        return phone;
-      } else {
-        cout << "ERROR: Invalid Input - Phone must be four digits (0-9)\n\n";
-      }
+            // Check for sentinel (-1)
+            if (phone == "-1") return "-1";
+
+            // Validate input
+            if (regex_match(phone, regex("[0-9]{4}"))) {
+                return phone;
+            } else {
+                cout << "ERROR: Invalid Input - Phone must be four digits only (0-9)\n\n";
+            }
+        }
     }
-  }
-}
+};
 
-// main() : Program execution starts here
+// MAIN METHOD : Program execution starts here
 int main() {
-  // Declare objects
-  Person p;
+    // Declare objects
+    Person p;
 
-  while (true) {
-    // Input & Process
-    string name = p.getName();
-    if (name == "-1") break;
-    string phone = p.getPhone();
-    if (phone == "-1") break;
+    while (true) {
+        // Input/Process
+        string name = p.getName();
+        if (name == "-1") break;
+        string phone = p.getPhone();
+        if (phone == "-1") break;
 
-    // Output
-    cout << "\nYour name:  " << name << endl;
-    cout << "Your phone: " << phone << << endl << endl;
-  }
+        // Write
+        ofstream outFile("myInfo.txt"); // Declare file object "outFile"
+        if (outFile.is_open()) {
+            outFile << name + " " + phone;
+            outFile.close();
+            // Output
+            cout << "CLEAR: \"myInfo.txt\" file recorded the following...\n\n";
+            cout << " Name: " << name << endl;
+            cout << "Phone: " << phone << endl << endl;
+        } else {
+            cout << "ERROR: File Not Found - Unable to open file for writing.\n\n";
+        }
+    }
 
-  return 0;      // End program
+    return 0;
 }
