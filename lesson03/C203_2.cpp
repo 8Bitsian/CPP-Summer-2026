8b105988a2215db1bccee717cda7c9876e0ad1cf
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-06-16 01:57:22 -0400

    Create C203_2.cpp

---
+++ lesson03/C203_2.cpp
@@ -0,0 +1,60 @@
+/*
+   Imani Hollie MM/DD/YYYY
+   5-Minute Interview: Title
+   Directions: Information
+*/
+
+/*
+	Imani Hollie MM/DD/YYYY
+	Lesson # Lab: Title
+	Directions: Information
+*/
+
+// HEADER
+#include <iostream>     // Provides console input/output
+#include <fstream>      // Provides file input/output
+#include <string>       // Enables use of std::string
+#include <regex>        // Enables regular expressions to check text
+using namespace std;    // Enables use of standard names w/out std:: prefix
+
+// CLASSES
+// ClassName{} : Description
+class ClassName {
+public:
+	// methodName() : Description
+	datatype methodName() {
+		// Input/Validate/Return
+   }
+};
+
+// FUNCTION PROTOTYPES
+datatype methodName(dataType parameter, etc.);
+
+// WRAPPER METHODS (PROCESS)
+// wrapMethod() : Description
+datatype wrapMethod() {
+	// Input/Validate/Return
+}
+
+// MAIN METHOD - main() : Program execution starts here
+int main() {
+	// Declare objects
+
+   while (true) {
+      // Input/Process
+      // Read/Write/Output
+   }
+
+   return 0;
+}
+
+// HELPER METHODS (GET & SET)
+// getMethod() : Description
+datatype getMethod() {
+	// Input/Validate/Return
+}
+
+// setMethod() : Description
+datatype setMethod() {
+	// Input/Validate/Return
+}
